d9b2fead494402e18b3a181f01e26d4e3b80e733
Md. Suman Sarkar <suman.iu32@gmail.com>
2024-10-27 20:59:44 +0600

    feat: add customer component design done

---
+++ src/components/customers/AddCustomer.jsx
@@ -0,0 +1,61 @@
+const AddCustomer = () => {
+    return (
+        <div
+            id="hs-stacked-overlays"
+            className="hs-overlay hidden hs-overlay-backdrop-open:bg-gray-900/50 size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
+            role="dialog"
+            tabIndex="-1"
+            aria-labelledby="hs-stacked-overlays">
+            <div className="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
+                <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-400 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                    <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
+                        <h3 id="hs-scale-animation-modal-label" className="font-bold text-gray-800 dark:text-white">
+                            Modal title
+                        </h3>
+                        <button
+                            type="button"
+                            className="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
+                            aria-label="Close"
+                            data-hs-overlay="#hs-stacked-overlays">
+                            <span className="sr-only">Close</span>
+                            <svg
+                                className="shrink-0 size-4"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round">
+                                <path d="M18 6 6 18"></path>
+                                <path d="m6 6 12 12"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="p-4 overflow-y-auto">
+                        <p className="mt-1 text-gray-800 dark:text-neutral-400">
+                            This is a wider card with supporting text below as a natural lead-in to additional content.
+                        </p>
+                    </div>
+                    <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
+                        <button
+                            type="button"
+                            className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                            data-hs-overlay="#hs-stacked-overlays">
+                            Close
+                        </button>
+                        <button
+                            type="button"
+                            className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                            Save changes
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AddCustomer;
